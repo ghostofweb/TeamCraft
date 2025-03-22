98986ba2267658fdfa606a38a49e052ccd76eb75
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-03-22 14:58:29 +0530

    Add NavBar component and integrate it into DashboardWrapper layout

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import NavBar from './(components)/NavBar'
 
-const DashBoardWrapper = ({children}:{children:React.ReactNode})=> {
+const DashboardWrapper = ({children}:{children:React.ReactNode}) => {
   return (
     <div className='flex min-h-screen w-full bg-gray-50 text-gray-900'>
-        {/* Sidebar */}
-        <main className={`flex w-full flex-col bg-grey-50 dark:bg-gray-900 md:pl-64`}>
-            {/* navbar */}
-        {children}
-        </main>
+    {/* SideBar */}
+    <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg md:pl-64`}>
+    {/* NavBar */}
+    <NavBar/>
+    {children}
+    </main>
     </div>
   )
 }
 
-export default DashBoardWrapper
+export default DashboardWrapper
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+    darkMode: 'class', // this means dark mode only applies when the 'dark' class is on the <html> element
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
